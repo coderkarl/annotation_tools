7af2f55a412dbe761c3a0c00a91d7f572fcfebf3
coderkarl <21990134+coderkarl@users.noreply.github.com>
2025-07-25 21:27:22 -0500

    Update rename_files.py

--- rename_files.py
+++ rename_files.py
@@ -3,14 +3,21 @@ import os
 import sys
 
 def rename_files_in_directory(directory):
+    base_name = "ai_image_set1_"
+    k = 0
     try:
         # Iterate through all files in the directory
-        for filename in os.listdir(directory):
+        for filename in sorted(os.listdir(directory)):
             # Check if the file contains a '-' to identify the prefix
             if '-' in filename:
                 # Split the filename at the first '-' and keep the part after it
                 new_name = filename.split('-', 1)[1]
+                if '-' in new_name:
+                    continue
                 # Construct full paths
+                #new_name = base_name + "%03d" % k + filename[filename.rfind('.'):]
+                # Increment the counter
+                k = k + 1
                 old_file = os.path.join(directory, filename)
                 new_file = os.path.join(directory, new_name)
                 # Rename the file
